83ff9a539924558a9fb054175cc994a758ea0343
fbognini <francesco.bognini@gmail.com>
2017-12-11 14:08:41 +0100

    Final commit

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+
+    // altervista
+    $databaseHost = 'localhost';
+    $databaseName = 'my_francescobognini';
+    $databaseUsername = '';
+    $databasePassword = '';
+
+    // server2go
+    /*
+    $databaseHost = 'localhost';
+    $databaseName = 'crud';
+    $databaseUsername = 'root';
+    $databasePassword = '';
+    */
+    
+    $mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
+    //@mysql_select_db("crud",$mysqli);
+?>
